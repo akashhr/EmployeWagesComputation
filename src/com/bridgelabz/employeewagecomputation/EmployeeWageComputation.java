b5211733acf75aee809d81102ef11d8a79d09549
Akash-gowda-hr <akashhr2040@gmail.com>
2021-08-23 16:53:27 +0530

    ADDED: employee monthly salary

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -3,14 +3,22 @@ package com.bridgelabz.employeewagecomputation;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		System.out.println("------------ Welcome to Employee Wage Computation! -------------");
-		int empPresesent = 1;
+System.out.println("------------ Welcome to Employee Wage Computation! -------------");
+		
+		int IS_PRESENT = 1;
+		int EMP_WAGE_PER_HOUR = 20;
+		
+		int empHrs, empWage;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == empPresesent){
-			System.out.println("Employee is Present");
+		if(empCheck == IS_PRESENT){
+			empHrs = 8;
 		}
 		else
-			System.out.println("Employee is Absent");
+			empHrs = 0;
+		
+		empWage = empHrs * EMP_WAGE_PER_HOUR;
+		System.out.println("Emp Hours : "+empHrs);
+		System.out.println("Emp Wage : "+empWage);
 
 	}
 
