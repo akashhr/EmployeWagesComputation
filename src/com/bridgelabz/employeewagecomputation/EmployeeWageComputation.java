bef2ab1b4bf9bfe9ba67e212e00763a48e26af7a
Akash-gowda-hr <akashhr2040@gmail.com>
2021-08-23 16:51:23 +0530

    ADDED:attendance check

--- src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecomputation/EmployeeWageComputation.java
@@ -3,7 +3,14 @@ package com.bridgelabz.employeewagecomputation;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("------------ Welcome to Employee Wage Computation! -------------");
+		int empPresesent = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == empPresesent){
+			System.out.println("Employee is Present");
+		}
+		else
+			System.out.println("Employee is Absent");
 
 	}
 
